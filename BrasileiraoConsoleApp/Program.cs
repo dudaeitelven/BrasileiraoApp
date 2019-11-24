3528d213e42e6a1b19881db06a9d2801b952314c
Gian Carlos <Gian Carlos@GIAN-PC>
2019-11-24 15:39:18 -0300

    Ajustes modo console

--- BrasileiraoConsoleApp/Program.cs
+++ BrasileiraoConsoleApp/Program.cs
@@ -8,18 +8,50 @@ using System.Threading.Tasks;
 
 namespace BrasileiraoConsoleApp
 {
-    class Program
+    public class Program
     {
-        
-
         static void Main(string[] args)
         {
+            int op = 1;
+
             Telas view = new Telas();
 
-            view.selecionaCampeonato();
-            view.telaPrincipal();
-            Console.ReadKey();
-            
+            while (op != 0)
+            {
+                op = view.telaPrincipal();
+                switch (op)
+                {
+                    case 1:
+                        Console.Clear();
+                        if (view.login() == true)
+                        {
+                            Console.Clear();
+                            view.areaRestrita();
+                        }
+                        else 
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Não logou");
+                        }
+                        break;
+                    case 2:
+                        Console.Clear();
+                        view.selecionaRodadas();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        view.selecionaEstatisticas();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        view.visualizaClassificacao();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        //Console.Clear();
+                        break;
+                }
+            }
         }
     }
 }
