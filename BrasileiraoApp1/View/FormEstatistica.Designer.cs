81604af995696583331a29f25a65e8ca0a40acf1
Eduardo Eitelven <dudaeitelven@gmail.com>
2019-11-24 23:13:35 -0300

    ok

--- BrasileiraoApp1/View/FormEstatistica.Designer.cs
+++ BrasileiraoApp1/View/FormEstatistica.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dataGridViewRodada = new System.Windows.Forms.DataGridView();
             this.grRodada = new System.Windows.Forms.GroupBox();
             this.cbRodada = new System.Windows.Forms.ComboBox();
             this.grCampeonato = new System.Windows.Forms.GroupBox();
@@ -38,28 +37,15 @@
             this.dataSetCAMPEONATOS = new BrasileiraoApp.DataSetCAMPEONATOS();
             this.dataSetCAMPEONATOSBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.cAMPEONATOTableAdapter = new BrasileiraoApp.DataSetCAMPEONATOSTableAdapters.CAMPEONATOTableAdapter();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRodada)).BeginInit();
+            this.groupBoxEstatistica = new System.Windows.Forms.GroupBox();
+            this.cbEstatistica = new System.Windows.Forms.ComboBox();
             this.grRodada.SuspendLayout();
             this.grCampeonato.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.cAMPEONATOBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetCAMPEONATOS)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetCAMPEONATOSBindingSource)).BeginInit();
+            this.groupBoxEstatistica.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // dataGridViewRodada
-            // 
-            this.dataGridViewRodada.AllowUserToAddRows = false;
-            this.dataGridViewRodada.AllowUserToDeleteRows = false;
-            this.dataGridViewRodada.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridViewRodada.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridViewRodada.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewRodada.Location = new System.Drawing.Point(12, 78);
-            this.dataGridViewRodada.Name = "dataGridViewRodada";
-            this.dataGridViewRodada.ReadOnly = true;
-            this.dataGridViewRodada.Size = new System.Drawing.Size(776, 360);
-            this.dataGridViewRodada.TabIndex = 5;
             // 
             // grRodada
             // 
@@ -138,31 +124,56 @@
             // 
             this.cAMPEONATOTableAdapter.ClearBeforeFill = true;
             // 
+            // groupBoxEstatistica
+            // 
+            this.groupBoxEstatistica.Controls.Add(this.cbEstatistica);
+            this.groupBoxEstatistica.Location = new System.Drawing.Point(425, 13);
+            this.groupBoxEstatistica.Name = "groupBoxEstatistica";
+            this.groupBoxEstatistica.Size = new System.Drawing.Size(200, 46);
+            this.groupBoxEstatistica.TabIndex = 6;
+            this.groupBoxEstatistica.TabStop = false;
+            this.groupBoxEstatistica.Text = "Selecione estatistica";
+            // 
+            // cbEstatistica
+            // 
+            this.cbEstatistica.AutoCompleteCustomSource.AddRange(new string[] {
+            "\'1\'",
+            "\'2\'",
+            "\'3\'",
+            "\'4\'",
+            "\'5\'"});
+            this.cbEstatistica.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbEstatistica.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbEstatistica.FormattingEnabled = true;
+            this.cbEstatistica.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.cbEstatistica.Location = new System.Drawing.Point(7, 20);
+            this.cbEstatistica.Name = "cbEstatistica";
+            this.cbEstatistica.Size = new System.Drawing.Size(187, 21);
+            this.cbEstatistica.TabIndex = 0;
+            // 
             // FormEstatistica
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridViewRodada);
+            this.Controls.Add(this.groupBoxEstatistica);
             this.Controls.Add(this.grRodada);
             this.Controls.Add(this.grCampeonato);
             this.Name = "FormEstatistica";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormEstatistica";
             this.Load += new System.EventHandler(this.FormEstatistica_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRodada)).EndInit();
             this.grRodada.ResumeLayout(false);
             this.grCampeonato.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.cAMPEONATOBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetCAMPEONATOS)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetCAMPEONATOSBindingSource)).EndInit();
+            this.groupBoxEstatistica.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridViewRodada;
         private System.Windows.Forms.GroupBox grRodada;
         private System.Windows.Forms.ComboBox cbRodada;
         private System.Windows.Forms.GroupBox grCampeonato;
@@ -171,5 +182,7 @@
         private DataSetCAMPEONATOS dataSetCAMPEONATOS;
         private System.Windows.Forms.BindingSource cAMPEONATOBindingSource;
         private DataSetCAMPEONATOSTableAdapters.CAMPEONATOTableAdapter cAMPEONATOTableAdapter;
+        private System.Windows.Forms.GroupBox groupBoxEstatistica;
+        private System.Windows.Forms.ComboBox cbEstatistica;
     }
 }
