525cb82388c2eaeeccff2d814e1de297ba560a81
Eduardo Eitelven <dudaeitelven@gmail.com>
2019-11-24 21:48:53 -0300

    ok

--- BrasileiraoApp1/View/FormCadastrarResultados.Designer.cs
+++ BrasileiraoApp1/View/FormCadastrarResultados.Designer.cs
@@ -29,31 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.rODADABindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSetCAMPEONATOS = new BrasileiraoApp.DataSetCAMPEONATOS();
+            this.cAMPEONATOBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cAMPEONATOTableAdapter = new BrasileiraoApp.DataSetCAMPEONATOSTableAdapters.CAMPEONATOTableAdapter();
+            this.rODADATableAdapter = new BrasileiraoApp.DataSetCAMPEONATOSTableAdapters.RODADATableAdapter();
+            this.dataGridViewRodada = new System.Windows.Forms.DataGridView();
             this.grRodada = new System.Windows.Forms.GroupBox();
             this.cbRodada = new System.Windows.Forms.ComboBox();
             this.grCampeonato = new System.Windows.Forms.GroupBox();
             this.cbCampeonato = new System.Windows.Forms.ComboBox();
-            this.dataSetCAMPEONATOS = new BrasileiraoApp.DataSetCAMPEONATOS();
-            this.cAMPEONATOBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.cAMPEONATOTableAdapter = new BrasileiraoApp.DataSetCAMPEONATOSTableAdapters.CAMPEONATOTableAdapter();
-            this.rODADABindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.rODADATableAdapter = new BrasileiraoApp.DataSetCAMPEONATOSTableAdapters.RODADATableAdapter();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.grRodada.SuspendLayout();
-            this.grCampeonato.SuspendLayout();
+            this.jOGOBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.jOGOTableAdapter = new BrasileiraoApp.DataSetCAMPEONATOSTableAdapters.JOGOTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.rODADABindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetCAMPEONATOS)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cAMPEONATOBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.rODADABindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRodada)).BeginInit();
+            this.grRodada.SuspendLayout();
+            this.grCampeonato.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.jOGOBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // rODADABindingSource
+            // 
+            this.rODADABindingSource.DataMember = "RODADA";
+            this.rODADABindingSource.DataSource = this.dataSetCAMPEONATOS;
+            // 
+            // dataSetCAMPEONATOS
+            // 
+            this.dataSetCAMPEONATOS.DataSetName = "DataSetCAMPEONATOS";
+            this.dataSetCAMPEONATOS.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cAMPEONATOBindingSource
+            // 
+            this.cAMPEONATOBindingSource.DataMember = "CAMPEONATO";
+            this.cAMPEONATOBindingSource.DataSource = this.dataSetCAMPEONATOS;
+            // 
+            // cAMPEONATOTableAdapter
+            // 
+            this.cAMPEONATOTableAdapter.ClearBeforeFill = true;
+            // 
+            // rODADATableAdapter
+            // 
+            this.rODADATableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridViewRodada
+            // 
+            this.dataGridViewRodada.AllowUserToAddRows = false;
+            this.dataGridViewRodada.AllowUserToDeleteRows = false;
+            this.dataGridViewRodada.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewRodada.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewRodada.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRodada.Location = new System.Drawing.Point(12, 78);
+            this.dataGridViewRodada.Name = "dataGridViewRodada";
+            this.dataGridViewRodada.ReadOnly = true;
+            this.dataGridViewRodada.Size = new System.Drawing.Size(776, 360);
+            this.dataGridViewRodada.TabIndex = 5;
             // 
             // grRodada
             // 
             this.grRodada.Controls.Add(this.cbRodada);
-            this.grRodada.Location = new System.Drawing.Point(218, 12);
+            this.grRodada.Location = new System.Drawing.Point(219, 13);
             this.grRodada.Name = "grRodada";
             this.grRodada.Size = new System.Drawing.Size(200, 46);
-            this.grRodada.TabIndex = 3;
+            this.grRodada.TabIndex = 4;
             this.grRodada.TabStop = false;
             this.grRodada.Text = "Selecione rodada";
             // 
@@ -65,8 +106,6 @@
             "\'3\'",
             "\'4\'",
             "\'5\'"});
-            this.cbRodada.DataSource = this.rODADABindingSource;
-            this.cbRodada.DisplayMember = "numeroRodada";
             this.cbRodada.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbRodada.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cbRodada.FormattingEnabled = true;
@@ -79,10 +118,10 @@
             // grCampeonato
             // 
             this.grCampeonato.Controls.Add(this.cbCampeonato);
-            this.grCampeonato.Location = new System.Drawing.Point(12, 12);
+            this.grCampeonato.Location = new System.Drawing.Point(13, 13);
             this.grCampeonato.Name = "grCampeonato";
             this.grCampeonato.Size = new System.Drawing.Size(200, 46);
-            this.grCampeonato.TabIndex = 2;
+            this.grCampeonato.TabIndex = 3;
             this.grCampeonato.TabStop = false;
             this.grCampeonato.Text = "Selecione campeonato";
             // 
@@ -104,72 +143,53 @@
             this.cbCampeonato.Name = "cbCampeonato";
             this.cbCampeonato.Size = new System.Drawing.Size(187, 21);
             this.cbCampeonato.TabIndex = 0;
+            this.cbCampeonato.ValueMember = "id";
+            this.cbCampeonato.SelectedIndexChanged += new System.EventHandler(this.cbCampeonato_SelectedIndexChanged);
             // 
-            // dataSetCAMPEONATOS
+            // jOGOBindingSource
             // 
-            this.dataSetCAMPEONATOS.DataSetName = "DataSetCAMPEONATOS";
-            this.dataSetCAMPEONATOS.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            this.jOGOBindingSource.DataMember = "JOGO";
+            this.jOGOBindingSource.DataSource = this.dataSetCAMPEONATOS;
             // 
-            // cAMPEONATOBindingSource
+            // jOGOTableAdapter
             // 
-            this.cAMPEONATOBindingSource.DataMember = "CAMPEONATO";
-            this.cAMPEONATOBindingSource.DataSource = this.dataSetCAMPEONATOS;
-            // 
-            // cAMPEONATOTableAdapter
-            // 
-            this.cAMPEONATOTableAdapter.ClearBeforeFill = true;
-            // 
-            // rODADABindingSource
-            // 
-            this.rODADABindingSource.DataMember = "RODADA";
-            this.rODADABindingSource.DataSource = this.dataSetCAMPEONATOS;
-            // 
-            // rODADATableAdapter
-            // 
-            this.rODADATableAdapter.ClearBeforeFill = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 373);
-            this.dataGridView1.TabIndex = 4;
+            this.jOGOTableAdapter.ClearBeforeFill = true;
             // 
             // FormCadastrarResultados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridViewRodada);
             this.Controls.Add(this.grRodada);
             this.Controls.Add(this.grCampeonato);
             this.Name = "FormCadastrarResultados";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormCadastrarResultados";
             this.Load += new System.EventHandler(this.FormCadastrarResultados_Load);
-            this.grRodada.ResumeLayout(false);
-            this.grCampeonato.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rODADABindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetCAMPEONATOS)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cAMPEONATOBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.rODADABindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRodada)).EndInit();
+            this.grRodada.ResumeLayout(false);
+            this.grCampeonato.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.jOGOBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox grRodada;
-        private System.Windows.Forms.ComboBox cbRodada;
-        private System.Windows.Forms.GroupBox grCampeonato;
-        private System.Windows.Forms.ComboBox cbCampeonato;
         private DataSetCAMPEONATOS dataSetCAMPEONATOS;
         private System.Windows.Forms.BindingSource cAMPEONATOBindingSource;
         private DataSetCAMPEONATOSTableAdapters.CAMPEONATOTableAdapter cAMPEONATOTableAdapter;
         private System.Windows.Forms.BindingSource rODADABindingSource;
         private DataSetCAMPEONATOSTableAdapters.RODADATableAdapter rODADATableAdapter;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewRodada;
+        private System.Windows.Forms.GroupBox grRodada;
+        private System.Windows.Forms.ComboBox cbRodada;
+        private System.Windows.Forms.GroupBox grCampeonato;
+        private System.Windows.Forms.ComboBox cbCampeonato;
+        private System.Windows.Forms.BindingSource jOGOBindingSource;
+        private DataSetCAMPEONATOSTableAdapters.JOGOTableAdapter jOGOTableAdapter;
     }
 }
--- BrasileiraoApp1/View/FormCadastrarResultados.cs
+++ BrasileiraoApp1/View/FormCadastrarResultados.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BrasileiraoApp.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,11 +20,33 @@ namespace BrasileiraoApp.View
 
         private void FormCadastrarResultados_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'dataSetCAMPEONATOS.RODADA' table. You can move, or remove it, as needed.
-            this.rODADATableAdapter.Fill(this.dataSetCAMPEONATOS.RODADA);
+            // TODO: This line of code loads data into the 'dataSetCAMPEONATOS.JOGO' table. You can move, or remove it, as needed.
+            this.jOGOTableAdapter.Fill(this.dataSetCAMPEONATOS.JOGO);
             // TODO: This line of code loads data into the 'dataSetCAMPEONATOS.CAMPEONATO' table. You can move, or remove it, as needed.
             this.cAMPEONATOTableAdapter.Fill(this.dataSetCAMPEONATOS.CAMPEONATO);
+            cbCampeonato.SelectedIndex = -1;
+        }
 
+        private void cbCampeonato_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillComboRodadas fillRodadas = new FillComboRodadas();
+            List<int> fillRodadasList = new List<int>();
+
+            //Limpa a seleção do combobox rodada
+            cbRodada.SelectedIndex = -1;
+            cbRodada.Items.Clear();
+
+            //Pegar o id selecionado no combo campeonato.
+            Object selectedValue = cbCampeonato.SelectedValue;
+
+            //Popular a List para montar o combo rodadas.
+            fillRodadasList = fillRodadas.RetornarRodadas(Convert.ToInt32(selectedValue));
+
+            //Montar o combo rodadas.
+            foreach (var lineListRodadas in fillRodadasList)
+            {
+                cbRodada.Items.Add(lineListRodadas.ToString());
+            }
         }
     }
 }
